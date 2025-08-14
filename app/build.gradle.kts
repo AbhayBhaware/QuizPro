7acb0098b26ed4e45586f3e2490731114ab660f7
AbhayBhaware <abhaybhaware20@gmail.com>
2025-08-15 01:11:06 +0530

    Login Page Added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,5 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     implementation("com.intuit.sdp:sdp-android:1.1.1")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
