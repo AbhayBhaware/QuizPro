3eaa469b3ba6a159d9cf879add3e106cc98da4a3
AbhayBhaware <abhaybhaware20@gmail.com>
2025-09-08 16:30:40 +0530

    Splash Screen Added

--- app/src/main/java/com/example/quizpro/SingupActivity.java
+++ app/src/main/java/com/example/quizpro/SingupActivity.java
@@ -3,12 +3,30 @@ package com.example.quizpro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
 
 public class SingupActivity extends AppCompatActivity {
+
+    EditText Ename, Enumber, Eemail, Epassword;
+    Button signupBTN;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singup);
+
+        Ename=findViewById(R.id.name);
+        Enumber=findViewById(R.id.number);
+        Eemail=findViewById(R.id.email);
+        Epassword=findViewById(R.id.password);
+
+        signupBTN=findViewById(R.id.signupBTN);
+
+
     }
 }
