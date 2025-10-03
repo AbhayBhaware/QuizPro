5747f018aef5cd62424a337765f01184be91b4b8
AbhayBhaware <abhaybhaware20@gmail.com>
2025-10-03 21:49:59 +0530

    UI chnages in signupActivity

--- app/src/main/java/com/example/quizpro/SingupActivity.java
+++ app/src/main/java/com/example/quizpro/SingupActivity.java
@@ -3,21 +3,28 @@ package com.example.quizpro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.FocusFinder;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.card.MaterialCardView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class SingupActivity extends AppCompatActivity {
 
     EditText Ename, Enumber, Eemail, Epassword;
     Button signupBTN;
 
+    MaterialCardView nameCard, numberCard, emailCard, passCard;
 
     DatabaseReference dbref;
 
@@ -31,8 +38,54 @@ public class SingupActivity extends AppCompatActivity {
         Eemail=findViewById(R.id.email);
         Epassword=findViewById(R.id.password);
 
+        nameCard=findViewById(R.id.nameCard);
+        numberCard=findViewById(R.id.mbCard);
+        emailCard=findViewById(R.id.emailCard);
+        passCard=findViewById(R.id.passwordCard);
+
+        List<MaterialCardView> allCard= Arrays.asList(nameCard, numberCard, emailCard, passCard);
+        List<EditText> allEdits=Arrays.asList(Ename, Enumber, Eemail, Epassword);
+
         signupBTN=findViewById(R.id.signupBTN);
         dbref= FirebaseDatabase.getInstance().getReference("Users");
+
+
+
+        View.OnClickListener selectCardListner = v ->{
+          for (MaterialCardView c: allCard)
+          {
+              c.setStrokeColor(Color.TRANSPARENT);
+          }
+          if (v instanceof EditText)
+          {
+              MaterialCardView parent =(MaterialCardView) v.getParent();
+
+              parent.setStrokeColor(Color.parseColor("#60CDC2"));
+          } else if (v instanceof MaterialCardView)
+          {
+              ((MaterialCardView) v).setStrokeColor(Color.parseColor("#60CDC2"));
+          }
+        };
+
+        for (MaterialCardView card:allCard)
+        {
+            card.setOnClickListener(selectCardListner);
+        }
+        for (EditText editText:allEdits)
+        {
+            editText.setOnFocusChangeListener((v, hasFocus)->{
+                if (hasFocus)
+                {
+                    for (MaterialCardView c:allCard)
+                    {
+                        c.setStrokeColor(Color.TRANSPARENT);
+                    }
+                    MaterialCardView parent =(MaterialCardView) v.getParent();
+                    parent.setStrokeColor(Color.parseColor("#60CDC2"));
+                }
+            });
+            editText.setOnClickListener(selectCardListner);
+        }
 
         signupBTN.setOnClickListener(new View.OnClickListener() {
             @Override
